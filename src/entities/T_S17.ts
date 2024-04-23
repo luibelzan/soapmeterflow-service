029fe17d81f9a3d65496f7a7a137d930d90142b5
Luis Miguel <luibelzan@outlook.com>
2024-04-23 11:25:16 +0200

    S06, S12, S14 ,S17 and S24 entities added

---
+++ src/entities/T_S17.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class T_S17 {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    cnc_id: string;
+
+    @Column()
+    fh: Date;
+
+    @Column()
+    et: number;
+
+    @Column()
+    c: number;
+
+    @Column()
+    d1: string;
+}
