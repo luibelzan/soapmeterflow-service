25ff6cf3f7f40ab1a6f83d31ae95765dc69f8865
Luis Miguel <luibelzan@outlook.com>
2025-02-26 14:23:35 +0100

    G05 insertion improved

--- src/controllers/xmlFileController.ts
+++ src/controllers/xmlFileController.ts
@@ -503,39 +503,47 @@ async function processG04(report: any, mag: number, reportDate: string, dataSour
 
 async function processG05(report: any, mag: number, reportDate: string, dataSource: DataSource): Promise<void> {
     const g05Repository = dataSource.getRepository(T_G05_TEMP);
-    for(const elem of report?.Cnc[0].Cnt) {
-        if(elem != undefined) {
-            for(let i=0; i<Object.keys(elem.G05).length; i++) {
-                try {
-                    var g05 = new T_G05_TEMP();
-                    g05.cnt_id = elem.$.Id;
-                    g05.fh = parseDate(elem.G05[i].$.Fh);
-                    g05.minvph1_lv = elem.G05[i].$.MinVph1_lv;
-                    g05.minvph2_lv = elem.G05[i].$.MinVph2_lv;
-                    g05.minvph3_lv = elem.G05[i].$.MinVph3_lv;
-                    g05.miniph1_lv = elem.G05[i].$.MinIph1_lv;
-                    g05.miniph2_lv = elem.G05[i].$.MinIph2_lv;
-                    g05.miniph3_lv = elem.G05[i].$.MinIph3_lv;
-                    g05.minpplus_triph = elem.G05[i].$.MinPplus_triph;
-                    g05.minpminus_triph = elem.G05[i].$.MinPminus_triph;
-                    g05.minqplus_triph = elem.G05[i].$.MinQplus_triph;
-                    g05.minqminus_triph = elem.G05[i].$.MinQminus_triph;
-                    g05.minvph1_mv = elem.G05[i].$.MinVph1_mv;
-                    g05.minvph2_mv = elem.G05[i].$.MinVph2_mv;
-                    g05.minvph3_mv = elem.G05[i].$.MinVph3_mv;
-                    g05.minineutral = elem.G05[i].$.MinIneutral;
-                    g05.minv0_comp = elem.G05[i].$.MinVo_comp;
-                    g05.minv1_comp = elem.G05[i].$.MinV1_comp;
-                    g05.minv2_comp = elem.G05[i].$.MinV2_comp;
-                    g05.minvhs = elem.G05[i].$.MinVhs;
-                    g05.bc = elem.G05[i].$.Bc;
-                    await g05Repository.save(g05);
-                    //console.log('G05 insertado');
-                } catch(err) {
-                    console.error(err);
+    let res = [];
+    const batchSize = 5000;
+    try {
+        for(const elem of report?.Cnc[0].Cnt) {
+            if(elem != undefined) {
+                for(let i=0; i<Object.keys(elem.G05).length; i++) {
+                        var g05 = new T_G05_TEMP();
+                        g05.cnt_id = elem.$.Id;
+                        g05.fh = parseDate(elem.G05[i].$.Fh);
+                        g05.minvph1_lv = elem.G05[i].$.MinVph1_lv;
+                        g05.minvph2_lv = elem.G05[i].$.MinVph2_lv;
+                        g05.minvph3_lv = elem.G05[i].$.MinVph3_lv;
+                        g05.miniph1_lv = elem.G05[i].$.MinIph1_lv;
+                        g05.miniph2_lv = elem.G05[i].$.MinIph2_lv;
+                        g05.miniph3_lv = elem.G05[i].$.MinIph3_lv;
+                        g05.minpplus_triph = elem.G05[i].$.MinPplus_triph;
+                        g05.minpminus_triph = elem.G05[i].$.MinPminus_triph;
+                        g05.minqplus_triph = elem.G05[i].$.MinQplus_triph;
+                        g05.minqminus_triph = elem.G05[i].$.MinQminus_triph;
+                        g05.minvph1_mv = elem.G05[i].$.MinVph1_mv;
+                        g05.minvph2_mv = elem.G05[i].$.MinVph2_mv;
+                        g05.minvph3_mv = elem.G05[i].$.MinVph3_mv;
+                        g05.minineutral = elem.G05[i].$.MinIneutral;
+                        g05.minv0_comp = elem.G05[i].$.MinVo_comp;
+                        g05.minv1_comp = elem.G05[i].$.MinV1_comp;
+                        g05.minv2_comp = elem.G05[i].$.MinV2_comp;
+                        g05.minvhs = elem.G05[i].$.MinVhs;
+                        g05.bc = elem.G05[i].$.Bc;
+                        res.push(g05);
+                        if(res.length >= batchSize) {
+                            await g05Repository.save(res);
+                            res = [];
+                        }
                 }
             }
         }
+        if(res.length > 0 ) {
+            await g05Repository.save(res);
+        }
+    } catch(err) {
+        console.error(err);
     }
 }
 
