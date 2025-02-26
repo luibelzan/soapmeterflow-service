179609bcaeefc7a6c82505c159e56f400b7b6639
Luis Miguel <luibelzan@outlook.com>
2025-02-26 15:11:00 +0100

    S67 insertion improved

--- src/controllers/xmlFileController.ts
+++ src/controllers/xmlFileController.ts
@@ -1608,21 +1608,30 @@ async function processS95(report: any, mag: number, reportDate: string, dataSour
 
 async function processS67(report: any, mag: number, reportDate: string, dataSource: DataSource): Promise<void> {
     const S67Repository = dataSource.getRepository(T_S67);
-    for(const elem of report?.Rtu) {
-        if(elem != undefined) {
-            for(let i=0; i<Object.keys(elem.S67).length; i++) {
-                try {
-                    var S67 = new T_S67();
-                    S67.rtu_id = elem.$.Id;
-                    S67.fh = parseDate(elem.S67[i].$.Fh);
-                    S67.et = elem.S67[i].$.Et;
-                    S67.c = elem.S67[i].$.C;
-                    S67.d1 = elem.S67[i].D1;
-                    await S67Repository.save(S67);
-                } catch(err) {
-                    console.error(err);
+    let res = [];
+    const batchSize = 5000;
+    try {
+        for(const elem of report?.Rtu) {
+            if(elem != undefined) {
+                for(let i=0; i<Object.keys(elem.S67).length; i++) {
+                        var S67 = new T_S67();
+                        S67.rtu_id = elem.$.Id;
+                        S67.fh = parseDate(elem.S67[i].$.Fh);
+                        S67.et = elem.S67[i].$.Et;
+                        S67.c = elem.S67[i].$.C;
+                        S67.d1 = elem.S67[i].D1;
+                        res.push(S67);
+                        if(res.length >= batchSize) {
+                            await S67Repository.save(res);
+                            res = [];
+                        }
                 }
             }
         }
+        if(res.length > 0) {
+            await S67Repository.save(res);
+        }
+    } catch(err) {
+        console.error(err);
     }
 }
